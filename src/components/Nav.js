3fd143d2a7e5257ed0f0a0c906fca6538a52f53b
bdavis90 <wowzerss90@yahoo.com>
2020-10-14 01:52:17 -0400

    finished nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+
+      <div className='nav'>
+        <h4 className='title'>Where in the world?</h4>
+        <h4 className='dark-mode'>Dark Mode</h4>
+      </div>
+
+  )
+}
+
+export default Nav
